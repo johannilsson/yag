1fa4694810eb340e3111ce789da5e8fe8a7a24bf
Johan Nilsson <johan@leo.(none)>
2008-11-02 12:21:22 +0100

    new helper

---
+++ application/views/helpers/PhotoLink.php
@@ -0,0 +1,33 @@
+<?php
+
+class Zend_View_Helper_PhotoLink
+{
+    /**
+     * Sets the view instance.
+     *
+     * @param  Zend_View_Interface $view View instance
+     * @return Zend_View_Helper_PaginationControl
+     */
+    public function setView(Zend_View_Interface $view)
+    {
+        $this->view = $view;
+        return $this;
+    }
+
+    /**
+     * Return the url to a photo
+     *
+     * @param  Zend_Db_Table_Row $photo
+     * @return string
+     */
+    public function photoLink($photo)
+    {
+        return $this->view->url(array(
+            'id'    => $photo->id, 
+            'year'  => date('Y', strtotime($photo->created_on)), 
+            'month' => date('m', strtotime($photo->created_on)), 
+            'day'   => date('d', strtotime($photo->created_on))
+        ), 'photo');
+    }
+
+}
