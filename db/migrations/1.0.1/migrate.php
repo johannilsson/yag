59f5cbad292e8e906a192f91aec59672f4fdd266
Johan Nilsson <johan@leo.(none)>
2008-11-23 11:01:59 +0100

    bug fixed script

--- db/migrations/1.0.1/migrate.php
+++ db/migrations/1.0.1/migrate.php
@@ -8,7 +8,7 @@ if (!isset($argv[1]) or !isset($argv[2])) {
 set_include_path($argv[1]);
 define('ENVIRONMENT', $argv[2]);
 
-require_once dirname(__FILE__) . '/../../application/bootstrap.php';
+require_once dirname(__FILE__) . '/../../../application/bootstrap.php';
 
 require_once APPLICATION_PATH . '/models/PhotoModel.php'; 
 $photoModel = new PhotoModel;
